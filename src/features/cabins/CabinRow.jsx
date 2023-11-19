996fbd1b0de8b6418b313259254ef8b49e840d89
jordansbenjamin <14327@coderacademy.edu.au>
2023-11-20 10:56:31 +1100

    Added & tested react-hot-toast

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
 	display: grid;
@@ -62,7 +63,7 @@ function CabinRow({ cabin }) {
 		mutationFn: deleteCabin,
 		// tell ReactQuery what to do as soon as mutation is successful
 		onSuccess: () => {
-			alert("Cabin successfully deleted");
+			toast.success("Cabin successfully deleted");
 			// So we want to re-fetch the data, this is done through:
 			// invalidating the cache as soon as mutation is done
 			queryClient.invalidateQueries({
@@ -70,7 +71,7 @@ function CabinRow({ cabin }) {
 				queryKey: ["cabins"],
 			});
 		},
-		onError: (err) => alert(err.message),
+		onError: (err) => toast.error(err.message),
 	});
 
 	return (
