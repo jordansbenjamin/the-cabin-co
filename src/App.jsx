9e301f5c229c723ef74c13a08132698948900322
jordansbenjamin <14327@coderacademy.edu.au>
2023-11-20 09:19:40 +1100

    Tested useQuery + spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -10,9 +12,24 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+// queryClient basically sets up the cache BTS
+const queryClient = new QueryClient({
+	defaultOptions: {
+		// options for queries
+		queries: {
+			// stale time is the amount of time
+			// the data in the cache will stay fresh
+			// or stay valid until it is re-fetched
+			staleTime: 60 * 1000,
+		},
+	},
+});
+
 function App() {
 	return (
-		<>
+		// Provide query data to entire app tree
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false}/>
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -34,7 +51,7 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 }
 
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,25 +1,53 @@
+/* eslint-disable no-unused-vars */
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
 
 const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
+	border: 1px solid var(--color-grey-200);
 
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
+	font-size: 1.4rem;
+	background-color: var(--color-grey-0);
+	border-radius: 7px;
+	overflow: hidden;
 `;
 
 const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
+	display: grid;
+	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+	column-gap: 2.4rem;
+	align-items: center;
 
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
+	background-color: var(--color-grey-50);
+	border-bottom: 1px solid var(--color-grey-100);
+	text-transform: uppercase;
+	letter-spacing: 0.4px;
+	font-weight: 600;
+	color: var(--color-grey-600);
+	padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+	const {
+		isLoading,
+		data: cabins,
+		error,
+	} = useQuery({
+		// queryKey uniquely identifies the data we are going to query
+		// has to be an array with a string or other complexities
+		queryKey: ["cabin"],
+		// queryFn is responsible for querying/fetching data from API
+		// specified func needs to return a promise
+		// whatever data is returned will be store in the cache
+		queryFn: getCabins,
+	});
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	return <div>Table</div>;
+}
+
+export default CabinTable;
