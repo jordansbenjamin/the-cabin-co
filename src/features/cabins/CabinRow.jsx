57338277c71584da5805d5a7d1bcbf0230a99839
jordansbenjamin <14327@coderacademy.edu.au>
2023-11-22 21:01:59 +1100

    Replaced buttons with icons

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -5,6 +5,9 @@ import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
+import { HiSquare2Stack } from "react-icons/hi2";
+import { HiPencil, HiTrash } from "react-icons/hi";
+import { useCreateCabin } from "./useCreateCabin";
 
 const TableRow = styled.div`
 	display: grid;
@@ -48,8 +51,20 @@ const Discount = styled.div`
 function CabinRow({ cabin }) {
 	const [showForm, setShowForm] = useState(false);
 	const { isDeleting, deleteCabin } = useDeleteCabin();
+	const { isCreating, createCabin } = useCreateCabin();
 
-	const { id: cabinId, name, maxCapacity, regularPrice, discount, image } = cabin;
+	const { id: cabinId, name, maxCapacity, regularPrice, discount, image, description } = cabin;
+
+	function handleDuplicate() {
+		createCabin({
+			name: `Copy of ${name}`,
+			maxCapacity,
+			regularPrice,
+			discount,
+			image,
+			description,
+		});
+	}
 
 	// ABSTRACTED AS A CUSTOM HOOK
 	// const queryClient = useQueryClient();
@@ -77,7 +92,6 @@ function CabinRow({ cabin }) {
 	// 	onError: (err) => toast.error(err.message),
 	// });
 
-
 	return (
 		<>
 			<TableRow role="row">
@@ -87,9 +101,14 @@ function CabinRow({ cabin }) {
 				<Price>{formatCurrency(regularPrice)}</Price>
 				{discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
 				<div>
-					<button onClick={() => setShowForm((show) => !show)}>Edit</button>
+					<button onClick={handleDuplicate}>
+						<HiSquare2Stack />
+					</button>
+					<button onClick={() => setShowForm((show) => !show)}>
+						<HiPencil />
+					</button>
 					<button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-						Delete
+						<HiTrash />
 					</button>
 				</div>
 			</TableRow>
