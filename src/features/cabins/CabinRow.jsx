857035736d492c44c0200d35ed6bc04860854d8c
jordansbenjamin <14327@coderacademy.edu.au>
2023-12-04 15:36:55 +1100

    Implemented modal for deleting cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -9,6 +9,7 @@ import { HiSquare2Stack } from "react-icons/hi2";
 import { HiPencil, HiTrash } from "react-icons/hi";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const TableRow = styled.div`
 	display: grid;
@@ -115,9 +116,15 @@ function CabinRow({ cabin }) {
 						<CreateCabinForm cabinToEdit={cabin} />
 					</Modal.Window>
 
-					<button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>
-						<HiTrash />
-					</button>
+					<Modal.Open>
+						<button>
+							<HiTrash />
+						</button>
+					</Modal.Open>
+
+					<Modal.Window>
+						<ConfirmDelete resourceName="cabins" disabled={isDeleting} onConfirm={() => deleteCabin(cabinId)}></ConfirmDelete>
+					</Modal.Window>
 				</Modal>
 			</div>
 		</TableRow>
